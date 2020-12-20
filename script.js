f3e2a5533ac687efe7e7b866c98c9527a05a6d4e
Jwhitehead-drft <67030406+Jwhitehead-drft@users.noreply.github.com>
2020-12-20 15:33:11 -0500

    Added comments

--- script.js
+++ script.js
@@ -1,151 +1,120 @@
-// Assignment Code
-// Clear the console on every refresh
-console.clear();
-var length = prompt("Select desired character count between 8 and 128")
-  if ((length > 128) ||
-  (length < 8)) {
-    alert("Please enter a value between 8 and 128");
-  }
+// Object containing arrays for possible characters and symbols
+var characters = {
+    "lowerCase": [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ],
+    "upperCase": [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ],
+    "number": [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+    ],
+    "symbol": [
+      " ",
+      "!",
+      "#",
+      "$",
+      "%",
+      "^",
+      "&",
+      "(",
+      ")",
+      "*",
+      "+",
+      ",",
+      "-",
+      ".",
+      "/",
+      ":",
+      ";",
+      "<",
+      ">",
+      "'",
+      "=",
+      '"',
+      "?",
+      "@",
+      "[",
+      "]",
+      "_",
+      "`",
+      "{",
+      "}",
+      "|",
+      "~",
+    ],
+  };
 
 
-// I want to create a password generator for 8-128 characters with selection -->
-     
-//     Get input to select if uppercase, numbers, special characters will be used -->
-function myFunction () {
-  var password = [];
-  var characters = {
-  "lowerCase": [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ],
-  "upperCase": [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  ],
-  "number": [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  ],
-  "symbol": [
-    " ",
-    "!",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    ":",
-    ";",
-    "<",
-    ">",
-    "'",
-    "=",
-    '"',
-    "?",
-    "@",
-    "[",
-    "]",
-    "_",
-    "`",
-    "{",
-    "}",
-    "|",
-    "~",
-  ],
-};
-var children = password.concat(specialCharacter)
-document.getElementById("").innerHTML = children;
-}
-
-
-
-//     Get input for length of password string
-
-//     function with arrays for character selection
-
-
-//     selected characters combined into one array
-
-//     math.random same length of string output.
-
-//    correlate math.random with possible characters 
-
-//     output to text area (line 21)
-//    DOM output for selected variables
 const uppercaseEl = document.getElementById("upperCase");
 const lowercaseEl = document.getElementById("lowerCase");
 const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// called by on click event line 200
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -153,17 +122,79 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// called by 118
+function generatePassword() {
+ 
+var password = ""
+var lowerCase = false
+var upperCase = false
+var numbers = false
+var symbol = false
+var randomIndex = 0
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-{
-	const length = +lengthEl.value;
-	const hasLower = lowercaseEl.checked;
-	const hasUpper = uppercaseEl.checked;
-	const hasNumber = numberEl.checked;
-	const hasSymbol = symbolEl.checked;
-	generatedPassword = true;
-  resultEl.innerText = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
+ // creates prompt for input and subsequent alerts for options
+  var length = prompt("Select desired character count between 8 and 128")
+  length=parseInt(length)
+  if ((length > 128) ||
+    (length < 8)) {
+    alert("Please enter a value between 8 and 128");
+  } else {
+    lowerCase = confirm("Use lower case?");
+    upperCase = confirm("Use upper case?");
+    numbers = confirm("Use numbers?");
+    symbol = confirm("Use symbols?");
+
+    for (var i = password.length; i < length; ) {
+      var shufflePassword = password.split("")
+      shufflePassword = shufflePassword.sort(function(){
+        return Math.random()-0.5
+      })
+      password = shufflePassword.join("")
+    if (lowerCase ){
+      randomIndex = Math.floor(Math.random()* characters.lowerCase.length)
+      password = password + characters.lowerCase[randomIndex]
+      i = password.length
+      if (i === length)
+      {
+        return password
+      }
+    } 
+    if (upperCase){
+      randomIndex = Math.floor(Math.random()* characters.upperCase.length)
+      password = password + characters.upperCase[randomIndex]
+      i = password.length
+      if (i === length)
+      {
+        return password
+      }
+    } 
+    if (numbers) {
+      randomIndex = Math.floor(Math.random()* characters.number.length)
+      password = password + characters.number[randomIndex]
+      i = password.length
+      if (i === length)
+      {
+        return password
+      }
+    }
+    if (symbol) {
+      randomIndex = Math.floor(Math.random()* characters.symbol.length)
+      password = password + characters.symbol[randomIndex]
+      i = password.length
+      if (i === length)
+      {
+        return password
+      }
+    }
+
+    
+
+  }
+
 }
 
+return password
 
+}
+
+generateBtn.addEventListener("click", writePassword);
